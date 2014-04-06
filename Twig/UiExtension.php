7155ddeab7acb4399b4276c45e31a6228e72a2f3
Etienne Dauvergne <contact@ekyna.com>
2014-04-06 14:41:41 +0200

    Form footer button offset

--- Twig/UiExtension.php
+++ Twig/UiExtension.php
@@ -100,7 +100,7 @@ class UiExtension extends \Twig_Extension
         )));
     }
 
-    public function renderFormFooter($path = null, $label = 'Enregistrer', $theme = 'primary', $icon = 'ok', $faIcon = false)
+    public function renderFormFooter($path = null, $label = 'Enregistrer', $theme = 'primary', $icon = 'ok', $faIcon = false, $offset = 3)
     {
         if(0 < strlen($icon)) {
             $icon = $faIcon ? 'fa fa-'.$icon : 'glyphicon glyphicon-'.$icon;
@@ -111,6 +111,7 @@ class UiExtension extends \Twig_Extension
             'label' => $label,
             'theme' => $theme,
             'icon' => $icon,
+            'offset' => $offset,
         ));
     }
 
